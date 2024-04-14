5980627114b3cdac5effb2d1333f4972f6f96fa6
k168ww <k168ww@gmail.com>
2024-04-14 23:09:25 +0900

    일단 커밋3

---
+++ src/main/java/com/SYVegas/chip/LogDTO.java
@@ -0,0 +1,14 @@
+package com.SYVegas.chip;
+
+import java.util.Date;
+
+public class LogDTO {
+
+    private int log_code;
+    private Date log_date;
+    private int log_money;
+    private String log_kind_money;
+    private String log_activity;
+    private String log_customer_id;
+
+}
