17974cc049050095e01852fd75b36e1e4fa024c8
GTU9 <107167326+GTU9@users.noreply.github.com>
2024-04-14 23:37:12 +0900

    풀리퀘전 커밋

--- src/main/java/com/SYVegas/common/Application.java
+++ src/main/java/com/SYVegas/common/Application.java
@@ -1,17 +1,7 @@
 package com.SYVegas.common;
 
-import com.SYVegas.check_customerInfoAndLog.Prototype;
-import com.SYVegas.playgame.PlayGame;
-
-import java.util.Scanner;
-
 public class Application {
     public static void main(String[] args) {
-//        Prototype prototype=new Prototype();
-//        prototype.managerPrototypeStart();
-        CurrentUser currentUser=new CurrentUser("김상억",2,3,4,6,15);
-        PlayGame playGame=new PlayGame();
-        playGame.gamePlay(currentUser);
 
     }
 }
