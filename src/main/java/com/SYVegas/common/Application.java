11227102b039d16c3d7582e0d7810f90c65ff22e
minsu <hangukin26@naver.com>
2024-04-14 23:37:30 +0900

    확인용

--- src/main/java/com/SYVegas/common/Application.java
+++ src/main/java/com/SYVegas/common/Application.java
@@ -1,20 +1,7 @@
 package com.SYVegas.common;
 
-import com.SYVegas.common.DepositAndPurchase.depositManager;
-import com.SYVegas.common.DepositAndPurchase.productPurchase;
-
-import java.util.Scanner;
-
 public class Application {
     public static void main(String[] args) {
 
-       depositManager DepositManager = new depositManager();
-       DepositManager.depositMoney();
-
-       productPurchase purchase = new productPurchase();
-       purchase.productPaymentType();
-
         }
-
-
     }
